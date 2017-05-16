72a4785cb4db972196970b467cd03aaa5372878a
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2017-05-16 12:28:59 +0200

    Added footer

--- common/fotocamere/highspeed.php
+++ common/fotocamere/highspeed.php
@@ -93,6 +93,7 @@
             </div>
         </main>
     </body>
+    <?php include("../component/footer.html"); ?>
     <!--Script-->
     <script src="../../js/jquery.min.js" type="text/javascript"></script>
     <script src="../../js/materialize.min.js" type="text/javascript"></script>
