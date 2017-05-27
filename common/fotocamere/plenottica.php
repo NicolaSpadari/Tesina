3ef8b7e1b3cc771a91ce988e917c5348ac147e33
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2017-05-27 17:10:48 +0200

    Code fix

--- common/fotocamere/plenottica.php
+++ common/fotocamere/plenottica.php
@@ -25,7 +25,6 @@
                 <div class="wrapper-banner">
                     <div class="container-banner">
                         <img class="img-banner" src="/images/plenoptic-banner.jpg">
-                        <h2 class="text-banner">Plenottica</h2>
                     </div>
                 </div>
             </div>
@@ -161,8 +160,8 @@
             </div>
         </main>
         <div id="btt" class="fixed-action-btn" style="bottom: 45px; right: 24px;">
-            <a class="btn-floating btn-large white waves-effect waves-light">
-                <i class="material-icons black-text">keyboard_arrow_up</i>
+            <a class="btn-floating btn-large grey lighten-2 waves-effect waves-light">
+                <i class="material-icons">keyboard_arrow_up</i>
             </a>
         </div>
     </body>
