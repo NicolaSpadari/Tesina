0a1da08c93597211fc463e59d1ee006dde734ef2
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2017-05-29 12:24:31 +0200

    Update subject with pdf images

--- common/materie/informatica.php
+++ common/materie/informatica.php
@@ -1,39 +1,19 @@
-<!DOCTYPE html>
-<html lang="it">
-    <head>
-        <title>Tesina di Spadari Nicola</title>
-    </head>
-    <body>
-        <main>
-            <div class="container">
-                <div class="section">
-                    <div class="row blocks">
-                        <div class="col s12 m4">
-                            <div class="icon-block">
-                                <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2>
-                                <h5 class="center">Speeds up development</h5>
-                                <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
-                            </div>
-                        </div>
-
-                        <div class="col s12 m4">
-                            <div class="icon-block">
-                                <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
-                                <h5 class="center">User Experience Focused</h5>
-                                <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
-                            </div>
-                        </div>
-
-                        <div class="col s12 m4">
-                            <div class="icon-block">
-                                <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
-                                <h5 class="center">Easy to work with</h5>
-                                <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </main>        
-    </body>
-</html>
+<div class="row">
+    <div class="center" style="padding-top: 1%; margin-bottom: 2%">
+        <a class="btn-floating waves-effect waves-light teal" type="button" onclick="prev();"><i class="material-icons">arrow_back</i></a>
+        <a class="btn-floating waves-effect waves-light teal" type="button" onclick="next();"><i class="material-icons">arrow_forward</i></a>
+    </div>
+    <div id="pdf" class="carousel carousel-slider">
+        <a class="carousel-item" href="#pag1"><img src="/images/pdf/0001.jpg"></a>
+        <a class="carousel-item" href="#pag2"><img src="/images/pdf/0002.jpg"></a>
+        <a class="carousel-item" href="#pag3"><img src="/images/pdf/0003.jpg"></a>
+        <a class="carousel-item" href="#pag4"><img src="/images/pdf/0004.jpg"></a>
+        <a class="carousel-item" href="#pag5"><img src="/images/pdf/0005.jpg"></a>
+        <a class="carousel-item" href="#pag6"><img src="/images/pdf/0006.jpg"></a>
+        <a class="carousel-item" href="#pag7"><img src="/images/pdf/0007.jpg"></a>
+        <a class="carousel-item" href="#pag8"><img src="/images/pdf/0008.jpg"></a>
+        <a class="carousel-item" href="#pag9"><img src="/images/pdf/0009.jpg"></a>
+        <a class="carousel-item" href="#pag10"><img src="/images/pdf/0010.jpg"></a>
+        <a class="carousel-item" href="#pag11"><img src="/images/pdf/0011.jpg"></a>
+    </div>
+</div>
