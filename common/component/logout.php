200e026f8bdc1562b34fec81c1e97caf0a688ea7
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2017-06-13 20:53:10 +0200

    Code fix

--- common/component/logout.php
+++ common/component/logout.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     session_destroy();    
-    if(basename($_SERVER['PHP_SELF']) == "users.php"){
+    if(basename($_SERVER['PHP_SELF']) == "users.php" || basename($_SERVER['PHP_SELF']) == "materie.php"){
         header("Location: /index.php");
     }else{
         if(basename($_SERVER['PHP_SELF']) != "logout.php"){
