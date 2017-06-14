255fbca128bd997fa1c460fc4c90765740852822
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2017-06-14 20:47:17 +0200

    Added world's fastest camera

--- common/materie/inglese.php
+++ common/materie/inglese.php
@@ -1,1 +1,144 @@
-
+<div class="container">
+    <div class="section">
+	<div class="row">
+	    <h3 id="titolo" class="col s12 light center header">The world's fastest camera</h3>
+	</div>
+	<div class="row">
+	    <div class="row blocks">
+		<div class="col s12 m12">
+		    <div class="icon-block">
+			<p class="light" style="text-align: center">
+			    In 2012 a team of scientists created something as nothing before: the trillion frames per second light-tracking camera.<br><br>
+			    The process, also named "femto-photography", consists of a continuous scanning of the object in order to create a single frame.
+			    To create a moving picture, a laser pulse was used (every 13 billionth of a second) to illuminate the scene; the laser 
+			    and the camera were synchronised to make sure each pulse was identical.<br>
+			    So when the light that hit the object is reflected, the camera shoots at that exact moment capturing the image.<br>
+			    MIT scientists say the process can have practical uses, such analyzing fault, ultrafast chemical processes and so on.
+			</p>
+		    </div>
+		</div>
+	    </div>
+	</div>
+	<div class="row">
+	    <div class="row blocks">
+		<div class="col s12 m5 offset-l1">
+		    <div class="icon-block">
+			<div class="row">
+			    <div class="col s12 m12">
+				<div class="card">
+				    <div class="card-content">
+					<h5 class="card-title activator grey-text text-darken-4" style="text-align: center">Light beam on</h5>
+				    </div>
+				    <div class="card-image">
+					<img src="/images/trillion-1.gif">
+				    </div>
+				    <div class="card-content">
+					<p class="card-title activator grey-text text-darken-4">
+					    A tomato recorded while a light beam passes on it.
+					</p>
+				    </div>
+				</div>
+			    </div>
+			</div>
+		    </div>
+		</div>
+		<div class="col s12 m5">
+		    <div class="icon-block">
+			<div class="row">
+			    <div class="col s12 m12">
+				<div class="card">
+				    <div class="card-content">
+					<h5 class="card-title activator grey-text text-darken-4" style="text-align: center">Light beam through</h5>
+				    </div>
+				    <div class="card-image">
+					<img src="/images/trillion-2.gif">
+				    </div>
+				    <div class="card-content">
+					<p class="card-title activator grey-text text-darken-4">
+					    A plastic bottle crossed by a light beam.
+					</p>
+				    </div>
+				</div>
+			    </div>
+			</div>
+		    </div>
+		</div>
+	    </div>
+	</div>
+	<br><br><div class="divider"></div><br><br>
+	<div class="row">
+	    <h3 id="titolo" class="col s12 light center header">The <b>new</b> world's fastest camera</h3>
+	</div>
+	<div class="row">
+	    <div class="row blocks">
+		<div class="col s12 m12">
+		    <div class="icon-block">
+			<p class="light" style="text-align: center">
+			    Years pass, and MIT’s 1-trillion-frame-per-second camera is now outdated. Researchers in Sweden have created a new world’s fastest camera that shoots 5 trillion frames per second.<br><br>
+			    This amazing new camera called FRAME can make light "practically stand still" and capture events as short as 0.2 trillionths of a second.
+			    Scientists say the camera will be able to capture things in chemistry, physics, biology, and medicine that have never been caught on camera before, on a femtosecond scale.
+			</p>
+		    </div>
+		</div>
+	    </div>
+	</div>
+	<br><br><div class="divider"></div><br><br>
+	<div class="row">
+	    <h3 id="titolo" class="col s12 light center header">A demonstration video</h3>
+	</div>
+	<div class="row">
+	    <div class="row blocks">
+		<div class="col s12 m12">
+		    <div class="icon-block">
+			<p class="light" style="text-align: center">
+			    Researchers shot video of light photons traveling the distance of the thickness of a sheet of paper.
+			    This only takes a picosecond, but the camera can show the photons traveling by slowing things down.
+			</p>
+		    </div>
+		</div>
+	    </div>
+	</div>
+	<div class="row">
+	    <div class="row blocks">
+		<div class="col s12 m8 offset-l2">
+		    <div class="video-container">
+			<iframe width="853" height="480" src="https://youtube.com/embed/2fjP02EpJac?vq=medium" frameborder="0"></iframe>
+		    </div>
+		</div>
+	    </div>
+	</div>
+	<br><br><div class="divider"></div><br><br>
+	<div class="row">
+	    <h3 id="titolo" class="col s12 light center header">Working</h3>
+	</div>
+	<div class="row">
+	    <div class="row blocks">
+		<div class="col s12 m12">
+		    <div class="icon-block">
+			<p class="light" style="text-align: center">
+			    Each light pulse recorded gives a unique code. The object reflects light flashes into a single photograph, separated using an encryption key;
+			    several coded images give the result of one picture when they are put together.
+			</p>
+		    </div>
+		</div>
+	    </div>
+	</div>
+	<div class="row">
+	    <div class="row blocks">
+		<div class="col s12 m4 offset-l4">
+		    <div class="icon-block">
+			<div class="row">
+			    <div class="col s12 m12">
+				<div class="card">
+				    <div class="card-image">
+					<img src="/images/photon.gif">
+				    </div>
+				</div>
+			    </div>
+			</div>
+		    </div>
+		</div>
+	    </div>
+	</div>
+    </div>
+</div>
