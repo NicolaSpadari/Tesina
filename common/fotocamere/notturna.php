b5acb2140e45711c8a37717533d30170d42fd70d
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2017-06-20 19:51:48 +0200

    Change banner location

--- common/fotocamere/notturna.php
+++ common/fotocamere/notturna.php
@@ -24,7 +24,7 @@
             <div class="banner">
                 <div class="wrapper-banner">
                     <div class="container-banner">
-                        <img class="img-banner" src="/images/night-banner.jpg">
+                        <img class="img-banner" src="/images/banner/night-banner.jpg">
                     </div>
                 </div>
             </div>
