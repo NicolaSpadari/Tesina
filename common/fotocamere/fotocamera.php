eebfdac29c39030950e2c5dbc35b7104176bd7df
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2017-06-20 19:50:55 +0200

    Change banner location

--- common/fotocamere/fotocamera.php
+++ common/fotocamere/fotocamera.php
@@ -24,7 +24,7 @@
             <div class="banner">
                 <div class="wrapper-banner">
                     <div class="container-banner">
-                        <img class="img-banner" src="/images/photo-banner.jpg">
+                        <img class="img-banner" src="/images/banner/photo-banner.jpg">
                     </div>
                 </div>
             </div>
